923a4d49b412cab68a269565f776dd0ffd1c5cce
luckschavalo <lucaschl1007@gmail.com>
2025-02-12 19:18:33 -0600

    real
    este es el real

---
+++ portafolioLucasChL/src/main/java/portalucas/portafolio/PortafolioLucasApplication.java
@@ -0,0 +1,13 @@
+package portalucas.portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioLucasApplication.class, args);
+	}
+
+}
